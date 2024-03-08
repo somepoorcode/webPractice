507bb2203a868412160af33fcf448501afb5e381
somepoorcode <119706110+somepoorcode@users.noreply.github.com>
2024-03-08 17:53:37 +0200

    Completed Task 1

--- lab1/code/index.php
+++ lab1/code/index.php
@@ -1,12 +1,14 @@
 <?php
 
-// task 1
+// Task 1
 /* Imagine a lot of code here */
 $very_bad_unclear_name = "15 chicken wings";
 
 // My code
-$order
+$order = & $very_bad_unclear_name;
+$order .= " and a soda";
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+
 
