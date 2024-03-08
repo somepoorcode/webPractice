e519e4aa6fed25b7b5efd8e1bace03ed4fa4e7c1
somepoorcode <119706110+somepoorcode@users.noreply.github.com>
2024-03-08 18:42:24 +0200

    Update index.php to look neater

--- lab1/code/index.php
+++ lab1/code/index.php
@@ -1,25 +1,24 @@
 <?php
 
+
 // Task 1
+echo "<b>"."Task 1"."</b>", "<br>";
 /* Imagine a lot of code here */
 $very_bad_unclear_name = "15 chicken wings";
-
 // My code
-$order = & $very_bad_unclear_name;
+$order = &$very_bad_unclear_name;
 $order .= " and a soda";
-
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
 
 
 // Task 2
+echo "<br>", "<b>"."Task 2"."</b>";
 $number = 15;
-echo "$number";
-echo "\n";
 $number1 = 14.325;
-echo "$number1";
 $number2 = 2.325;
-echo "\n", $number1-$number2;
+echo "<br>", "$number\n$number2\n", $number1 - $number2;
 $last_month = 1187.23;
 $this_month = 1089.98;
 echo "<br>", "Разница в расходах между прошлым и текущим месяцем: ", $last_month - $this_month, ".";
+
