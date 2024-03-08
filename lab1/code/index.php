59e00605f3235af3d3edbabd88c991cf19fc5586
somepoorcode <119706110+somepoorcode@users.noreply.github.com>
2024-03-08 17:39:45 +0200

    Started Task 1

--- lab1/code/index.php
+++ lab1/code/index.php
@@ -1,2 +1,12 @@
 <?php
-echo phpinfo();
+
+// task 1
+/* Imagine a lot of code here */
+$very_bad_unclear_name = "15 chicken wings";
+
+// My code
+$order
+
+// Don't change the line below
+echo "\nYour order is: $very_bad_unclear_name.";
+
