e1c423b06473cf32498559e7682b84a1f2a81ea6
somepoorcode <119706110+somepoorcode@users.noreply.github.com>
2024-03-30 21:50:43 +0200

    Completed Task 1 A

--- lab3/code/index.php
+++ lab3/code/index.php
@@ -1,2 +1,9 @@
 <?php
 echo "<b>"."Task 1. Regular expressions"."</b>", "<br />";
+echo "<b>"."A"."<b>", "<br />";
+$str = 'abba acda acdca aboba ava';
+$regExp = '/a..a/';
+$matches = [];
+$count = preg_match_all($regExp, $str, $matches);
+echo "Количество найденных строк: {$count}", "<br />";
+var_dump($matches);
