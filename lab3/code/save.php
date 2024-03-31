7ebbe8003e1102fa171ce44aaff4cda45caf280a
somepoorcode <119706110+somepoorcode@users.noreply.github.com>
2024-03-31 22:02:16 +0200

    Updated save.php to Save More Data

--- lab3/code/save.php
+++ lab3/code/save.php
@@ -10,12 +10,14 @@ if (false === isset($_POST['mail'], $_POST['category'], $_POST['title'], $_POST[
     getOutNow();
 }
 
+$mail = $_POST['mail'];
 $category = $_POST['category'];
 $title = $_POST['title'];
 $description = $_POST['description'];
+$fileContains = "$mail\n$category\n$title\n$description";
 
 $filePath = "./categories/{$category}/{$title}.txt";
-if (false === file_put_contents($filePath, $description)) {
+if (false === file_put_contents($filePath, $fileContains)) {
     throw new Exception('Something went wrong.');
 }
 chmod($filePath, 0777);
