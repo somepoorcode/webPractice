de568e423b65ff5514c735e2e922c45738c5d17b
somepoorcode <119706110+somepoorcode@users.noreply.github.com>
2024-05-03 22:40:31 +0200

    Modified save.php to add data to spreedsheet

--- lab4/code/save.php
+++ lab4/code/save.php
@@ -1,26 +1,33 @@
 <?php
-function getOutNow(): void
-{
-    header('Location: /');
-    exit();
-}
+require __DIR__ . '/vendor/autoload.php';
 
+$client = new \Google_Client();
+$client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+$client->setAuthConfig(__DIR__ . '/key.json');
 
-if (false === isset($_POST['mail'], $_POST['category'], $_POST['title'], $_POST['description'])) {
-    getOutNow();
-}
+$service = new Google_Service_Sheets($client);
 
-$mail = $_POST['mail'];
-$category = $_POST['category'];
+$spreadsheetId = '16wMqqQoSJlNddFLXJ-Y7SXs-flDv3z6e124R640c1Y8';
+$range = 'Лист1';
+
+$email = $_POST['mail'];
+$category = ucfirst($_POST['category']);
 $title = $_POST['title'];
 $description = $_POST['description'];
-$fileContains = "$mail\n$category\n$title\n$description";
 
-$filePath = "./categories/{$category}/{$title}.txt";
-if (false === file_put_contents($filePath, $fileContains)) {
-    throw new Exception('Something went wrong.');
-}
-chmod($filePath, 0777);
-getOutNow();
+$values = [
+    [$email, $category, $title, $description]
+];
 
-?>
+$body = new Google_Service_Sheets_ValueRange([
+    'values' => $values
+]);
+
+$params = [
+    'valueInputOption' => 'RAW'
+];
+
+$result = $service->spreadsheets_values->append($spreadsheetId, $range, $body, $params);
+
+header('Location: index.php');
+exit;
